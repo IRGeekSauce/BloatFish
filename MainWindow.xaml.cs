43c4ecb7a9cb9300d52709a52b7af49cff9bc357
Christopher Sheridan <christopher_sheridan83@yahoo.com>
2017-12-20 14:11:20 -0600

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -354,11 +354,16 @@ namespace BloatFish
                 }, null);
             }
         }
-
+       /*
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
+        }
+        */
+        private void storeLaunchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("ms-windows-store:home");
         }
     }
 }
